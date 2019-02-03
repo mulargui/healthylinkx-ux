230a55ce51c20516bc71d89154673603b30db967
mulargui <mauricio@ulargui.com>
2019-02-03 08:52:31 -0800

    refining setup

--- k8s/update.bat
+++ k8s/update.bat
@@ -1,5 +1,7 @@
 
-rem just update the pages or js and relaunch the app
-rem nothing else to do
+rem if you updated the html pages or js just relaunch the app
+rem else we cleanup and redeploy the service
+call %~dp0.\cleanup.bat
+call %~dp0.\setup.bat
 
 exit /B 0
